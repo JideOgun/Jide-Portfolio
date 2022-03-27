8076807a939ddecfc649422ce3b45d9b0998986e
JideOgun <JideOgun@github.com>
2022-03-27 13:25:29 -0500

    added contact links to footer

--- jides-portfolio/src/components/Footer.js
+++ jides-portfolio/src/components/Footer.js
@@ -5,7 +5,10 @@ import React from 'react';
 function Footer () {
     return ( 
         <div className='footer' bg="primary">
-    Footer
+        <div><a href="https://www.linkedin.com/in/jide-ogunbanjo/">LinkedIn</a></div>
+   
+      <div> <a href="https://www.github.com/jideogun">Github</a></div>
+     
         </div>
      );
 }
